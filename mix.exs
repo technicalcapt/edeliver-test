c7a9db25e3183e1ca061f5691b1e8b2342728a98
technicalcapt <technicalcapt@gmail.com>
2016-08-02 15:15:56 +0700

    add edeliver dependency

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule EdeliverTest.Mixfile do
   def application do
     [mod: {EdeliverTest, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :edeliver]]
   end
 
   # Specifies which paths to compile per environment.
@@ -37,6 +37,7 @@ defmodule EdeliverTest.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
+     {:edeliver, ">= 0.0.0"},
      {:cowboy, "~> 1.0"}]
   end
 
